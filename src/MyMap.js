24f7cf70b68e3313af8c13145efbdde2fdf33375
Emilio Marcos <emilio7marcos@gmail.com>
2023-01-25 00:36:42 +0000

    add first map markers

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Map from "react-map-gl"
+import Map, { Marker } from "react-map-gl"
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
@@ -10,14 +10,26 @@ function MyMap() {
       initialViewState={{
         longitude: 115.1,
         latitude: -8.6,
-        zoom: 10
+        zoom: 9.5
       }}
       style={{width: 800, height: 600}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       // Public token
       mapboxAccessToken={"pk.eyJ1IjoiZW1pbGlvbWFyY29zIiwiYSI6ImNsZGFkOHViODBoN2szb3N6bmRnYnZjcm4ifQ.foYqfhSQ1_tk_3FXm2eKBw"}
-    />
+    >
+      <Marker longitude={115.1359} latitude={-8.6638} anchor="bottom" />
+      <Marker longitude={115.1301} latitude={-8.6595} anchor="bottom" />
+      <Marker longitude={115.1254} latitude={-8.6551} anchor="bottom" />
+      <Marker longitude={115.1215} latitude={-8.6518} anchor="bottom" />
+    </Map>
   );
 }
 
 export default MyMap;
+
+// Marked beaches in order:
+
+// Berawa
+// Batu Bolong
+// Echo
+// Perenan
