c722662ef49b7e02af30b48e43abf78520a3dbb1
Emilio Marcos <emilio7marcos@gmail.com>
2023-01-26 17:30:30 +0000

    add markers through data

--- src/App.js
+++ src/App.js
@@ -16,20 +16,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="AppContainer">
-        <MapProvider>
-          <MyMap />
-        </MapProvider>
+          <MapProvider>
+            <MyMap />
+          </MapProvider>
         <h1>Surf Spots 🏄‍♂️</h1>
         <div className="surf-spots">
           {spots}
-          {/* <h3>🌊 Berawa: </h3>
-            <img src="https://i.ytimg.com/vi/XTuRFOLBzbU/maxresdefault.jpg" alt="Berawa" className="spot-img"/>
-          <h3>🌊 Batu Bolong: </h3>
-            <img src="https://i.ytimg.com/vi/vSOlFG_v9JQ/maxresdefault.jpg" alt="Batu Bolong" className="spot-img"/>
-          <h3>🌊 Echo: </h3>
-            <img src="https://i.ytimg.com/vi/0eAxg5tHtMk/maxresdefault.jpg" alt="Echo" className="spot-img"/>
-          <h3>🌊 Pererenan: </h3>
-            <img src="https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg" alt="Perenan" className="spot-img"/> */}
         </div>
       </div>
     </div>
--- src/MyMap.js
+++ src/MyMap.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Map, { Marker } from "react-map-gl"
 import 'mapbox-gl/dist/mapbox-gl.css'
+import data from "./data";
 
 
 function MyMap() {
-  // console.log((115.1301 + 115.1254) / 2);
-  // console.log((8.6595 + 8.6551) / 2);
+  const markers = data.map(spot => {
+    return (
+      <Marker
+        longitude={spot.longitude}
+        latitude={spot.latitude}
+        anchor="bottom"
+      />
+    )
+  })
   return (
     <Map
       id="mymap"
@@ -19,19 +27,9 @@ function MyMap() {
       // Public token
       mapboxAccessToken={"pk.eyJ1IjoiZW1pbGlvbWFyY29zIiwiYSI6ImNsZGFkOHViODBoN2szb3N6bmRnYnZjcm4ifQ.foYqfhSQ1_tk_3FXm2eKBw"}
     >
-      <Marker longitude={115.1359} latitude={-8.6638} anchor="bottom" />
-      <Marker longitude={115.1301} latitude={-8.6595} anchor="bottom" />
-      <Marker longitude={115.1254} latitude={-8.6551} anchor="bottom" />
-      <Marker longitude={115.1215} latitude={-8.6518} anchor="bottom" />
+      {markers}
     </Map>
   );
 }
 
 export default MyMap;
-
-// Marked beaches in order:
-
-// Berawa
-// Batu Bolong
-// Echo
-// Pererenan
