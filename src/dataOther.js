2d419c33a4588f71e87b25b9b7a7ad59528885eb
Emilio Marcos <emilio7marcos@gmail.com>
2023-01-27 10:54:11 +0000

    add data file

---
+++ src/dataOther.js
@@ -0,0 +1,94 @@
+const dataOther = [
+  {
+    id: 1,
+    title: "Padang Padang",
+    location: "Uluwatu, Bali",
+    latitude: -8.6638,
+    longitude: 115.1359,
+    description: "Sandy beach break mixed with reefs, Beginner/intermediate level.",
+    imageUrl: "https://i.ytimg.com/vi/XTuRFOLBzbU/maxresdefault.jpg",
+    forecast: "• Forecast: 6-9ft  /  16s  /  12mph offshore"
+  },
+  {
+    id: 2,
+    title: "Ulu",
+    location: "Uluwatu, Bali",
+    latitude: -8.6595,
+    longitude: 115.1301,
+    description: "Sandy beach break perfect for beginners. Very popular spot, may be crowded.",
+    imageUrl: "https://i.ytimg.com/vi/vSOlFG_v9JQ/maxresdefault.jpg",
+    forecast: "• Forecast: 6-9ft  /  16s  /  12mph offshore"
+  },
+  {
+    id: 3,
+    title: "Balangan",
+    location: "Uluwatu, Bali",
+    latitude: -8.6551,
+    longitude: 115.1254,
+    description: "Reef break more suitable for advanced surfers. Hot spot for competitions.",
+    imageUrl: "https://i.ytimg.com/vi/0eAxg5tHtMk/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Bingin",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Green Bowl",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Dreamland",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Impossibles",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Nyang Nyang",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  },
+  {
+    id: 4,
+    title: "Thomas Beach",
+    location: "Uluwatu, Bali",
+    latitude: -8.6518,
+    longitude: 115.1215,
+    description: "Reef break more suitable for advanced surfers. Possibly beginners when small.",
+    imageUrl: "https://i.ytimg.com/vi/sXHrYzeh8Ro/maxresdefault.jpg",
+    forecast: "• Forecast: 4-6ft  /  12s  /  8mph offshore"
+  }
+]
+
+export default dataOther;
