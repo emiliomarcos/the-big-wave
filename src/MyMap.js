6960c8d4f0bfd77a2503b5b64168aff14bb0e083
Emilio Marcos <emilio7marcos@gmail.com>
2023-01-27 12:25:38 +0000

    replace display city

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Map, { Marker } from "react-map-gl"
 import 'mapbox-gl/dist/mapbox-gl.css'
-import data from "./data";
+import dataOther from "./dataOther";
 
 
 function MyMap() {
-  const markers = data.map(spot => {
+  const markers = dataOther.map(spot => {
     return (
       <Marker
         longitude={spot.longitude}
@@ -17,14 +17,19 @@ function MyMap() {
   return (
     <Map
       id="mymap"
+      // initialViewState={{
+      //   longitude: 115.1277,
+      //   latitude: -8.6573,
+      //   zoom: 13
+      // }}
       initialViewState={{
-        longitude: 115.1277,
-        latitude: -8.6573,
-        zoom: 13
+        longitude: 115.0884,
+        latitude: -8.8149,
+        zoom: 12
       }}
       style={{width: 800, height: 400}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-      // mapboxAccessToken={}
+      mapboxAccessToken={"pk.eyJ1IjoiZW1pbGlvbWFyY29zIiwiYSI6ImNsZGFkOHViODBoN2szb3N6bmRnYnZjcm4ifQ.foYqfhSQ1_tk_3FXm2eKBw"}
     >
       {markers}
     </Map>
--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 export default function Video() {
-    return (
-        <div>
-            <iframe title="video" width="800" height="400" src="https://www.youtube.com/embed/pdGqoO-s_sg?autoplay=1&amp;mute=1&amp;controls=0">
-            </iframe>
-        </div>
-    )
+  return (
+    <div>
+      {/* <iframe title="video" width="800" height="400" src="https://www.youtube.com/embed/pdGqoO-s_sg?autoplay=1&amp;mute=1&amp;controls=0">
+      </iframe> */}
+      <iframe title="video" width="800" height="400" src="https://www.youtube.com/embed/js258P4f1-s?autoplay=1&amp;mute=1&amp;controls=0">
+      </iframe>
+    </div>
+  )
 }
